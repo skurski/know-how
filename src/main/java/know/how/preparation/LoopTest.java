33effaf421a3cf63ae8a90571f11b270d2a9980b
PiotrSkurski <Pskurski@luxoft.com>
2016-07-06 14:40:47 +0200

    Added loop testing

---
+++ src/main/java/know/how/preparation/LoopTest.java
@@ -0,0 +1,23 @@
+package know.how.preparation;
+
+/**
+ * Loops testing...
+ */
+public class LoopTest {
+
+    public static void main(String[] args) {
+
+        short c = (short) 5;
+        switch(c) {
+            case 5:
+                System.out.println("5");
+                break;
+            case 10:
+                System.out.println("10");
+                break;
+            default:
+                System.out.println("default");
+        }
+
+    }
+}
